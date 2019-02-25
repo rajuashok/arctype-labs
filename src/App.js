720ef1c0f5f0110b1afd4c2a097806e9bb54dedc
Ian Mendiola <ianmendiola@gmail.com>
2019-02-25 14:15:17 -0800

    more landing page and some styling

--- src/App.js
+++ src/App.js
@@ -22,15 +22,22 @@ class App extends Component {
         <section className="App-title">
           <div>
             <h1>A new and better way to communicate with your distributed team</h1>
-            <h2>Fast, reliable and simple voice chat for teams — Create your own voice channels and organize them by team, project or whatever you’d like</h2>
+            <div className="App-title-subtitle">
+              <h2>Fast, reliable and simple voice chat for teams — Create your own voice channels and organize them by team, project or whatever you’d like</h2>
+            </div>
+            <div className="App-get-started">
+              <a href="#" className="large-button">Get Started</a>
+            </div>
           </div>
         </section>
         <section className="App-image">
         </section>
         <section className="App-benefits">
-          <Benefits title="Feel connected" description="Quickly jump into a voice channel anytime and start talking with everyone that is there, as if you’re in the same room" />
-          <Benefits title="Make decisions faster" description="Stay in sync and get right to talking instead of needlessly trying to have meaningful discussions over text" />
-          <Benefits title="Make talking frictionless" description="No more installing apps, sending around links, entering PINs or choppy calls.  Reliable audio in a beautiful modern interface in your browser." />
+          <div className="App-benefits-container">
+            <Benefits title="Feel connected" description="Quickly jump into a voice channel anytime and start talking - as if you’re in the same room." />
+            <Benefits title="Make decisions faster" description="Stay in sync and get right to talking instead of needlessly trying to have meaningful discussions over text." />
+            <Benefits title="Talk with ease" description="No more installing apps, sending around links or entering PINs. Reliable audio in your browser." />
+          </div>
         </section>
       </div>
 
