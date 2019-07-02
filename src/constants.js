0e8b3e18a84d9738080ad8ad3b94c57ca04c2799
Ashok Raju <deville75@gmail.com>
2019-07-02 14:08:50 -0400

    Add color span to feature titles

--- src/constants.js
+++ src/constants.js
@@ -1,24 +1,26 @@
+import React from 'react';
+
 export const arctypeFeatures = [
   {
-    title: "Arctype SQL",
+    title: <span>Arctype <span className="text-green">SQL</span></span>,
     description: "The next-generation SQL client built for collaboration",
     icon: "/sql-icon.svg",
     disabled: false
   },
   {
-    title: "Arctype Dataprep",
+    title: <span>Arctype <span className="text-orange">Dataprep</span></span>,
     description: "Intelligent visual data preparation and date cleaning",
     icon: "/dataprep-icon.svg",
     disabled: true
   },
   {
-    title: "Arctype Datasets",
+    title: <span>Arctype <span className="text-blue">Datasets</span></span>,
     description: "Share and manage datasets across teams",
     icon: "/datasets-icon.svg",
     disabled: true
   },
   {
-    title: "Arctype ML",
+    title: <span>Arctype <span className="text-pink">ML</span></span>,
     description: "No-code machine learning",
     icon: "/ml-icon.svg",
     disabled: true
