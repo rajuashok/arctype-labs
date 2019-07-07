f2be257f5c7c33aa3023d602157bb0f1f92cc68c
AreTea <tbusinessr@gmail.com>
2019-07-07 14:56:24 -0400

    adding bottom keys to background

--- src/App.js
+++ src/App.js
@@ -294,6 +294,7 @@ class App extends Component {
       <div>
         {this.renderTop()}
         <div className="App-hero-robot"/>
+        <div className="Hero-robot-weights" />
         {this.renderFeatures()}
         {this.renderBottomBox()}
         {this.renderCustomSolutions()}
